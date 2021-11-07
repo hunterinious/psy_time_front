513af549ce2eab6235d54b1c12aa0a1767829908
Vladyslav Nerodenko <v.nerodenko@pnn.in.ua>
2021-11-07 17:17:44 +0200

    base implementation of footer

--- psy_time/src/components/Footer/Footer.js
+++ psy_time/src/components/Footer/Footer.js
@@ -3,10 +3,27 @@ import styles from './Footer.module.scss';
 
 const Header = (props) => {
     return (
-        <footer>
-            <div className="container mt-5">
-                <div className="d-flex justify-content-center">
-                    Здесь будет футер
+        <footer className={styles.Footer}>
+            <div className={styles.FooterContent}>
+                <div className={styles.FooterItem}>
+                    <p className={styles.FooterItemTitle}>
+                        Psy Time
+                    </p>
+                    <ul className={styles.FooterItemTitleList}>
+                        <li>
+                            About project
+                        </li>
+                    </ul>
+                </div>
+                <div className={styles.FooterItem}>
+                    <p className={styles.FooterItemTitle}>
+                        Contacts
+                    </p>
+                    <ul className={styles.FooterItemTitleList}>
+                        <li>
+                            contact@psytime.com
+                        </li>
+                    </ul>
                 </div>
             </div>
         </footer>
